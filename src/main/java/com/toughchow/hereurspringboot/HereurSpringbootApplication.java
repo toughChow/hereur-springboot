ec15eb54febe8e3e81482150731993b120231707
zhoufaming <zhoufaming@iyuyun.net>
2019-05-14 19:16:04 +0800

    Initial commit

---
+++ src/main/java/com/toughchow/hereurspringboot/HereurSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.toughchow.hereurspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereurSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HereurSpringbootApplication.class, args);
+    }
+
+}
